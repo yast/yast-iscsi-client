479fcc512be38d8f74cd863845156c7c724d6405
Gabriele Mohr <gs@suse.de>
2013-10-15 14:12:19 +0200

    test case added

--- test/ipEqual_spec.rb
+++ test/ipEqual_spec.rb
@@ -33,22 +33,34 @@ describe Yast::IscsiClientLibClass do
         expect(@iscsilib.ipEqual?("[213:23:]", "...")).to be_false
       end
     end
-    context "with equal and valid IPv6 arguments (without port)" do
+    context "with equal (but different string) and valid IPv6 arguments (without port)" do
       it "returns true" do
         expect(@iscsilib.ipEqual?("[2620:0113:1c0:8080:4ec:544a:000d:3d62]",
                                   "[2620:113:1c0:8080:4ec:544a:d:3d62]")).to be_true
       end
     end
-    context "with equal IPv6 arguments and different ports" do
-      it "returns false" do
-        expect(@iscsilib.ipEqual?("[2620:113:80c0:8080:54ec:544a:3b9d:3d62]:456",
-                                  "[2620:113:80c0:8080:54ec:544a:3b9d:3d62]:4")).to be_false
+    context "with equal (same string) and valid IPv6 arguments (without port)" do
+      it "returns true" do
+        expect(@iscsilib.ipEqual?("[2620:113:1c0:8080:4ec:544a:000d:3d62]",
+                                  "[2620:113:1c0:8080:4ec:544a:d:3d62]")).to be_true
       end
     end
-    context "with equal IPv6 arguments and equal ports" do
+    context "with equal (but different string) and valid IPv6 arguments and equal ports" do
       it "returns true" do
         expect(@iscsilib.ipEqual?("[0020:0113:80c0:8080:0:544a:3b9d:3d62]:456",
                                   "[20:113:80c0:8080::544a:3b9d:3d62]:456")).to be_true
+      end
+    end
+    context "with equal (but different string) IPv6 arguments and different ports" do
+      it "returns false" do
+        expect(@iscsilib.ipEqual?("[2620:0113:80c0:8080:54ec:004a:3b9d:3d62]:456",
+                                  "[2620:113:80c0:8080:54ec:4a:3b9d:3d62]:4")).to be_false
+      end
+    end
+    context "with equal (same string) IPv6 arguments and different ports" do
+      it "returns false" do
+        expect(@iscsilib.ipEqual?("[2620:113:80c0:8080:54ec:544a:3b9d:3d62]:456",
+                                  "[2620:113:80c0:8080:54ec:544a:3b9d:3d62]:4")).to be_false
       end
     end
   end

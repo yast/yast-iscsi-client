59b0e1b6afee193848445f3ef71b18e62caba1b6
David Díaz <dgonzalez@suse.de>
2018-08-03 14:24:20 +0100

    Improve test from code review

--- test/iscsi_client_lib_test.rb
+++ test/iscsi_client_lib_test.rb
@@ -6,8 +6,6 @@ describe Yast::IscsiClientLibClass do
   subject { described_class.new }
 
   describe "#getServiceStatus" do
-    let(:iscsid_socket) { double("iscsid.socket", start: true) }
-
     before do
       allow(Yast::Service).to receive(:Stop).with(anything)
       allow(Yast::Service).to receive(:active?).with(anything)
@@ -16,54 +14,60 @@ describe Yast::IscsiClientLibClass do
       allow(subject.log).to receive(:error)
     end
 
-    context "when iscsid.socket and iscsid.service are active" do
-      before do
-        allow(Yast::Service).to receive(:active?).with("iscsid").and_return(true)
-        allow(Yast::SystemdSocket).to receive(:find!).with("iscsid").and_return(iscsid_socket)
-        allow(subject).to receive(:socketActive?).with(iscsid_socket).and_return(true)
-      end
+    context "for iscsid.socket" do
+      let(:iscsid_socket) { double("iscsid.socket", start: true) }
 
-      it "does nothing" do
-        expect(iscsid_socket).to_not receive(:start)
-        expect(Yast::Service).to_not receive(:Stop).with("iscsid")
-
-        subject.getServiceStatus
-      end
-    end
-
-    context "when iscsid.socket is not active" do
       before do
         allow(Yast::SystemdSocket).to receive(:find!).with("iscsid").and_return(iscsid_socket)
-        allow(subject).to receive(:socketActive?).with(iscsid_socket).and_return(false)
+        allow(subject).to receive(:socketActive?).with(iscsid_socket).and_return(socket_status)
       end
 
-      context "and socket is availbale" do
-        it "start iscsid.socket" do
-          expect(iscsid_socket).to receive(:start)
+      context "when iscsid.socket and iscsid.service are active" do
+        let(:socket_status) { true }
+
+        before do
+          allow(Yast::Service).to receive(:active?).with("iscsid").and_return(true)
+        end
+
+        it "does not start the socket nor the service" do
+          expect(iscsid_socket).to_not receive(:start)
+          expect(Yast::Service).to_not receive(:Stop).with("iscsid")
 
           subject.getServiceStatus
         end
+      end
 
-        context "but iscsid.service is active" do
-          before do
-            allow(Yast::Service).to receive(:active?).with("iscsid").and_return(true)
-          end
+      context "when iscsid.socket is not active" do
+        let(:socket_status) { false }
 
-          it "stop iscsid.service" do
-            expect(Yast::Service).to receive(:Stop).with("iscsid")
+        context "and socket is availbale" do
+          it "starts iscsid.socket" do
+            expect(iscsid_socket).to receive(:start)
 
             subject.getServiceStatus
           end
+
+          context "but iscsid.service is active" do
+            before do
+              allow(Yast::Service).to receive(:active?).with("iscsid").and_return(true)
+            end
+
+            it "stops iscsid.service" do
+              expect(Yast::Service).to receive(:Stop).with("iscsid")
+
+              subject.getServiceStatus
+            end
+          end
         end
-      end
 
-      context "but socket is not availbale" do
-        let(:iscsid_socket) { nil }
+        context "but socket is not availbale" do
+          let(:iscsid_socket) { nil }
 
-        it "logs an error" do
-          expect(subject.log).to receive(:error).with("Cannot start iscsid.socket")
+          it "logs an error" do
+            expect(subject.log).to receive(:error).with("Cannot start iscsid.socket")
 
-          subject.getServiceStatus
+            subject.getServiceStatus
+          end
         end
       end
     end
